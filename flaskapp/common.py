93cc1a798f137d80f4017b45ba0bd56233cf44e1
Rafael Amadigi Dal Santo <rafaeldalsanto@gmail.com>
2019-01-06 17:28:41 -0200

    Memoize

--- flaskapp/common.py
+++ flaskapp/common.py
@@ -1,2 +1,19 @@
+import weakref
+from functools import wraps
+
+
 def merge_dicts(base, other):
     return dict(base, **other) if other is not None else base
+
+
+def memoize(fn):
+    cache = weakref.WeakKeyDictionary()
+
+    @wraps(fn)
+    def wrap(self):
+        if self in cache:
+            return cache[self]
+        value = cache[self] = fn(self)
+        return value
+
+    return wrap
